7e1ac9c41afdd4edd0641d53bd6a373ab6e7de32
pimoumeister <fouquetarthur@gmail.com>
2024-09-26 13:17:44 +0200

    Add files via upload

---
+++ Hello_World.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void){ // commentaire test
+	printf("Hello\n");
+	return 0;
+}
+
---
+++ test.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main (){
+unsigned char a=1;
+signed int b=-2;
+printf("%d", ~a);
+}
